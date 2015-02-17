ac4bd61faf9716cb8674c840e8f57da958eea788
Jason Whitehouse <jdb175@gmail.com>
2015-02-16 19:14:17 -0500

    Added start to side bar chart

--- js/main.js
+++ js/main.js
@@ -13,13 +13,18 @@
   10. Mitoses                       1 - 10
   11. Class:                        (2 for benign, 4 for malignant)
 */
-	var cancerCount = 0;
-	var benignCount = 0;
+var cancerCount = 0;
+var benignCount = 0;
+var totalCancerCount;
+var totalBenignCount;
+var svg;
+var barG;
 
 window.onload = function () {
 
 	var margin = {top: 30, right: 10, bottom: 10, left: 10},
-		    width = 960 - margin.left - margin.right,
+			barWidth = 100,
+		    width = 960 - barWidth - margin.left - margin.right,
 		    height = 500 - margin.top - margin.bottom;
 
 	var x = d3.scale.ordinal().rangePoints([0, width], 1),
@@ -31,13 +36,17 @@ window.onload = function () {
 	    background,
 	    foreground;
 
-	var svg = d3.select("body").append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-	  .append("g")
+	svg = d3.select("body").append("svg")
+	    .attr("width", width + barWidth + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom);
+
+	barG = svg.append("g")
+		.attr("transform", "translate(" + (margin.left+width) + "," + margin.top + ")");
+
+	svg = svg.append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-   d3.csv("cancer_filtered.csv", function(error, cancer) {
+   d3.csv("cancer_full.csv", function(error, cancer) {
 
 		// Extract the list of dimensions and create a scale for each.
 		x.domain(dimensions = d3.keys(cancer[0]).filter(function(d) {
@@ -55,6 +64,7 @@ window.onload = function () {
 		  .attr("d", path);
 
 		// Add blue foreground lines for focus.
+		// Also count cancerous and benign
 		foreground = svg.append("g")
 		.selectAll("path")
 		  .data(cancer)
@@ -69,6 +79,8 @@ window.onload = function () {
 		  	}
 		  })
 		  .attr("d", path);
+		  totalCancerCount = cancerCount;
+		  totalBenignCount = benignCount;
 
 		// Add a group element for each dimension.
 		var g = svg.selectAll(".dimension")
@@ -101,42 +113,46 @@ window.onload = function () {
 		          .attr("visibility", null);
 		    }));
 
-		// Add an axis and title.
-		g.append("g")
-		  .attr("class", "axis")
-		  .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-		.append("text")
-		  .style("text-anchor", "middle")
-		  .attr("y", -9)
-		  .text(function(d) { return d; });
+			// Add an axis and title.
+			g.append("g")
+			  .attr("class", "axis")
+			  .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+			.append("text")
+			  .style("text-anchor", "middle")
+			  .attr("y", -9)
+			  .text(function(d) { return d; });
 
-		// Add and store a brush for each axis.
-		g.append("g")
-		  .attr("class", "brush")
-		  .each(function(d) {
-		    d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
-		  })
-		.selectAll("rect")
-		  .attr("x", -8)
-		  .attr("width", 16);
+			// Add and store a brush for each axis.
+			g.append("g")
+			  .attr("class", "brush")
+			  .each(function(d) {
+			    d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
+			  })
+			.selectAll("rect")
+			  .attr("x", -8)
+			  .attr("width", 16);
+
+		 	//Draw bars
+			drawBars();
+
 		});
 
 		function position(d) {
-		var v = dragging[d];
-		return v == null ? x(d) : v;
+			var v = dragging[d];
+			return v == null ? x(d) : v;
 		}
 
 		function transition(g) {
-		return g.transition().duration(500);
+			return g.transition().duration(500);
 		}
 
 		// Returns the path for a given data point.
 		function path(d) {
-		return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+			return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
 		}
 
 		function brushstart() {
-		d3.event.sourceEvent.stopPropagation();
+			d3.event.sourceEvent.stopPropagation();
 		}
 
 		// Handles a brush event, toggling the display of foreground lines.
@@ -146,10 +162,48 @@ window.onload = function () {
 			var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
 			  extents = actives.map(function(p) { return y[p].brush.extent(); });
 			foreground.style("display", function(d) {
-			return actives.every(function(p, i) {
-			  return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-			}) ? null : "none";
-		});
-	}
+				var shown = actives.every(function(p, i) {
+					return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+				})
+				if(shown) {
+					if(d.Class=="4") {
+						cancerCount++;
+					} else {
+						benignCount++;
+					}
+					return null;
+				} else {
+					return "none";
+				}
+			});
+			drawBars();
+		}
+
+		//Draws the side bar chart
+		function drawBars() {
+			var total = totalCancerCount + totalBenignCount;
+			var data = [(totalCancerCount-cancerCount), cancerCount, benignCount, (totalBenignCount-benignCount)];
+			var cScale = color = d3.scale.ordinal().range(["#EDD1D1", "#F08080", "#4682B4", "#8FA1B0"]).domain([0,1,2,3]);
+			var bound = barG.selectAll("rect")
+				.data(data)
+			bound.enter()
+				.append("rect")
+				.attr("width", width)
+			bound.transition().attr("height", function(d) { 
+					if(total == 0) { 
+						return 0 
+					} else { 
+						return height * d/total 
+					}
+				})
+				.attr("y", function(d,i) {
+					var y = 0;
+					for(var j = i-1; j >=0 ; --j){
+						y += data[j] * height/total;
+					}
+					return y;
+				})
+				.attr("fill", function(d,i) { return cScale(i)});
+		}
 
 }
